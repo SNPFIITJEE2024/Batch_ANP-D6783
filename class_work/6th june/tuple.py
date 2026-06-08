ef84c9a2f8c0b7617f07599b1ac95113b4e80e44
TANUJ BHAWARIYA <gokultanuj2005@gmail.com>
2026-06-08 22:30:55 +0530

    8th june commit

--- class_work/6th june/tuple.py
+++ class_work/6th june/tuple.py
@@ -29,8 +29,6 @@ students = (
 print("----- Student's Records -----")
 for record in students:
     print("Student id : ", record[0])
-    print("Name : ", record[1])
-    print("Course : ", record[2])
     print("Fee Paid : Rs", record[3])
     print("------------------------------")
 
